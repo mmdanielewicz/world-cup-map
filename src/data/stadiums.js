2e7bc52e627a9a87a1a929ac03e183f3a8588c95
Magdalena Danielewicz <danielewiczmaggie@gmail.com>
2026-06-14 15:20:19 -0400

    add js of stadium names, coords, location

---
+++ src/data/stadiums.js
@@ -0,0 +1,18 @@
+export const stadiums = [
+    { name: "MetLife Stadium", city: "East Rutherford, NJ", country: "USA", lat: 40.8136, lng: -74.0744 },
+    { name: "AT&T Stadium", city: "Arlington, TX", country: "USA", lat: 32.7473, lng: -97.0945 },
+    { name: "SoFi Stadium", city: "Inglewood, CA", country: "USA", lat: 33.9535, lng: -118.3392 },
+    { name: "Levi's Stadium", city: "Santa Clara, CA", country: "USA", lat: 37.4032, lng: -121.9698 },
+    { name: "Arrowhead Stadium", city: "Kansas City, MO", country: "USA", lat: 39.0489, lng: -94.4839 },
+    { name: "Lincoln Financial Field", city: "Philadelphia, PA", country: "USA", lat: 39.9008, lng: -75.1675 },
+    { name: "Mercedes-Benz Stadium", city: "Atlanta, GA", country: "USA", lat: 33.7553, lng: -84.4006 },
+    { name: "NRG Stadium", city: "Houston, TX", country: "USA", lat: 29.6847, lng: -95.4107 },
+    { name: "Hard Rock Stadium", city: "Miami Gardens, FL", country: "USA", lat: 25.9580, lng: -80.2389 },
+    { name: "Gillette Stadium", city: "Foxborough, MA", country: "USA", lat: 42.0909, lng: -71.2643 },
+    { name: "Lumen Field", city: "Seattle, WA", country: "USA", lat: 47.5952, lng: -122.3316 },
+    { name: "Estadio Azteca", city: "Mexico City", country: "Mexico", lat: 19.3029, lng: -99.1505 },
+    { name: "Estadio BBVA", city: "Monterrey", country: "Mexico", lat: 25.6694, lng: -100.2444 },
+    { name: "Estadio Akron", city: "Guadalajara", country: "Mexico", lat: 20.6797, lng: -103.4621 },
+    { name: "BC Place", city: "Vancouver", country: "Canada", lat: 49.2768, lng: -123.1119 },
+    { name: "BMO Field", city: "Toronto", country: "Canada", lat: 43.6332, lng: -79.4186 }
+  ];
