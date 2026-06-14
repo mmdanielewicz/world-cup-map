c0876baf47110a10d8588fc82d9d922ad38a42ee
Magdalena Danielewicz <danielewiczmaggie@gmail.com>
2026-06-14 16:39:28 -0400

    test displaying stadium info when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import './App.css'
 import StadiumMap from './components/StadiumMap'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // keep track of which staidum is clicked on
+  const [selectedStadium, setSelectedStadium] = useState(null)
 
   return (
     <div>
       <div className="container">
-        <img src={fifaLogo} alt="FIFA Logo" className="fifa-logo" 
-          style={{ width: '100%', maxWidth: '700px', height: 'auto' }}/>
 
+        <div className="header">
+          <img src={fifaLogo} alt="FIFA Logo" className="fifa-logo" />
+        </div>
 
-        <StadiumMap />
+        <pre>{JSON.stringify(selectedStadium, null, 2)}</pre>
+
+        <StadiumMap onStadiumClick={setSelectedStadium}/>
       </div>
     </div>
   )
--- src/components/StadiumMap.jsx
+++ src/components/StadiumMap.jsx
@@ -3,24 +3,32 @@ import L from 'leaflet';
 import { stadiums } from '../data/stadiums';
 import stadiumIconImg from '../assets/stadium.png';
 
+// custom stadium marker on map
 const stadiumIcon = L.icon({
     iconUrl: stadiumIconImg,
-    iconSize: [32, 32],      
+    iconSize: [48, 48],      
     iconAnchor: [16, 32],    
     popupAnchor: [0, -32], 
     className: 'stadium-icon'
   })
 
-function StadiumMap() {
+// leaflet map component
+function StadiumMap({ onStadiumClick }) {
   return (
-    <MapContainer center={[35, -97]} zoom={4} style={{ height: "600px", width: "100%" }}>
+    <MapContainer center={[35, -97]} zoom={4} style={{ height: "800px", width: "100%" }}>
         <TileLayer
-            attribution="&copy; OpenStreetMap"
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; OpenStreetMap &copy; CARTO'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
 
+        
         {stadiums.map((stadium) => (
-            <Marker key={stadium.name} position={[stadium.lat, stadium.lng]} icon={stadiumIcon}>
+            <Marker key={stadium.name} position={[stadium.lat, stadium.lng]} icon={stadiumIcon}
+                eventHandlers={{
+                    click: () => 
+                        onStadiumClick(stadium),
+                }}>
+
                 <Popup>
                     <strong>{stadium.name}</strong><br />
                     {stadium.city}, {stadium.country}
